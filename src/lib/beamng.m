ed57562703ec048868a1531f581e9ff58ef1e96e
Patrick Schrangl <pschrangl@beamng.gmbh>
2025-02-20 14:30:52 +0100

    Add entrypoint file

---
+++ src/lib/beamng.m
@@ -0,0 +1,8 @@
+function beamng()
+%BEAMNG  BeamNG.tech Support for MATLAB and Simulink
+%- Open <a href="https://documentation.beamng.com/api/matlab-simulink/">Documentation</a>
+%- View <a href="https://github.com/BeamNG/BeamNG-MATLAB-Simulink-integration">Repository</a>
+
+% This file is just an entrypoint to the documentation.
+help beamng
+end
