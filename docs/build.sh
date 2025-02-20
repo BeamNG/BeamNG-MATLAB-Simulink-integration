ac64e33af0af8717bc8015a63c43c062f76eb39e
Patrick Schrangl <pschrangl@beamng.gmbh>
2025-02-20 08:59:54 +0100

    Start adding documentation

--- docs/build.sh
+++ docs/build.sh
@@ -6,4 +6,19 @@ set -eo pipefail
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR"
 
-python test.py
+# Add relevant branches and tags locally
+git fetch origin --tags
+git fetch origin main:main
+if [ "$USE_DEV_BRANCH" == "true" ]; then
+    git fetch origin dev:dev
+    BRANCH="dev"
+else
+    BRANCH="main"
+fi
+
+# Build docs
+rm -rf build
+pip install -r requirements.txt
+git config --global --add safe.directory $(dirname "$PWD")
+sphinx-multiversion source build
+python generate_index.py $BRANCH build/index.html
---
+++ docs/source/index.rst
@@ -0,0 +1,11 @@
+BeamNG.tech Support for MATLAB and Simulink
+===========================================
+
+Welcome to the documentation of the BeamNG integration for MATLAB and Simulink.
+
+.. toctree::
+   :maxdepth: 1
+   :caption: Contents
+
+   Introduction <readme.rst>
+   changelog.rst
