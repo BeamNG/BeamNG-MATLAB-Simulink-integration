f79c40b7de6a97997832727d4b0e9b00661d1222
Patrick Schrangl <pschrangl@beamng.gmbh>
2025-02-27 18:44:56 +0100

    Remove unnecessary algebraic loop around controller in examples

--- src/examples/simulink/S-function/controller.m
+++ src/examples/simulink/S-function/controller.m
@@ -1,33 +1,33 @@
-function [throttle,yaw_angle,torque,steering] = controller(throttle,...
-    speed_threshold,speed_sim,rad_angle,...
-    throttle_input_for_steering_controller,steering_threshold,steering)
+function [throttle,torque,steering] = controller(...
+    speed_desired,speed,yaw_angle_rad,...
+    steering_control,yaw_angle_desired)
 % Controller for S-function examples.
 
 % Steering
-steering_clearance=1; % in degrees
-% here's converting the angle from radiant to degree
-yaw_angle=rad_angle*(180/pi);
-if yaw_angle <= (steering_threshold-steering_clearance)
-    steering=-throttle_input_for_steering_controller;
-end
-
-if  yaw_angle > (steering_threshold)
-    steering=throttle_input_for_steering_controller;
+yaw_clearance = 1; % in degrees
+yaw_angle = yaw_angle_rad*(180/pi); % rad to degree
+if yaw_angle <= (yaw_angle_desired-yaw_clearance)
+    steering = -steering_control;
+elseif yaw_angle > (yaw_angle_desired)
+    steering = steering_control;
+else
+    steering = 0;
 end
 
 % Throttle and Torque
 speed_clearance=10;
-actual_speed=speed_sim*3.6;
+actual_speed=speed*3.6;
 if actual_speed <= 5
     torque = 270 ;
-    throttle=0.5;
-elseif actual_speed <= (speed_threshold-speed_clearance)
-    throttle=0.5;
+    throttle = 0.5;
+elseif actual_speed <= (speed_desired-speed_clearance)
+    throttle = 0.5;
     torque = 200 ;
-elseif  actual_speed > (speed_threshold)
-    throttle=-0.05;
+elseif  actual_speed > (speed_desired)
+    throttle = -0.05;
     torque = 65;
 else
+    throttle = 0;
     torque = 170;
 end
 end
